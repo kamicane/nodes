a2f5d3ec6a6bc202985a03c0b4d993d915d2dcf7
Valerio Proietti <kamicane@gmail.com>
2014-06-25 02:26:24 +0200

    better es6 style exports

--- index.js
+++ index.js
@@ -3,9 +3,12 @@
 require('./lib/finder');
 var types = require('./types');
 var factory = require('./lib/factory');
+var syntax = require('./syntax.json');
 
 var build = factory.build;
-build.nodes = types;
-build.lists = factory.lists;
+var lists = factory.lists;
 
-module.exports = build;
+exports.default = exports.build = build;
+exports.syntax = syntax;
+exports.nodes = types;
+exports.lists = lists;
